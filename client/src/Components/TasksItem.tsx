7caabf1e6c76ee953515aa4de7095a3aa13d2312
Otep Olzhas <olzhasktl362@gmail.com>
2023-04-13 01:15:56 +0600

    Creating main.tsx, register.tsx, login.tsx, index.tsx

---
+++ client/src/Components/TasksItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TasksItem(props) {
+    return (
+        <h2>Task 1</h2>
+    );
+}
+
+export default TasksItem;
---
+++ client/src/pages/register.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from '@/styles/Register.module.css'
+
+function register(props) {
+    return (
+        <h1 className={classes.button}>Register page</h1>
+    );
+}
+
+export default register;
