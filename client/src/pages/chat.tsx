c958930f352ba4424be9179ebcb67eb09844a65e
Otep Olzhas <olzhasktl362@gmail.com>
2023-05-27 13:49:49 +0600

    feat: Video chat

---
+++ client/src/pages/chat.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AgoraChat from "@/Components/Lobby/AgoraChat"
+
+function Chat(props) {
+    return (
+        <div>
+            <AgoraChat/>
+        </div>
+    );
+}
+
+export default Chat;
--- client/src/pages/lobby.tsx
+++ client/src/pages/lobby.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import Lobby from '@/Components/Lobby/Lobby'
+import React, {useState} from 'react';
+import dynamic from "next/dynamic";
+const VideoRoom = dynamic(() => import('../Components/Lobby/VideoRoom'), { ssr: false });
+
 
 function lobby(props) {
     return (
         <div>
-            <Lobby/>
+            <VideoRoom/>
         </div>
     );
 }
